64285a43bacbd808bcc560ddc08102d7231aa9b3
Jen Hoskins <jen.c.hoskins@gmail.com>
2020-10-12 16:54:32 +0100

    Remove default layout styling, put header and footer in header and footer components, add said components to layout component

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "gatsby"
+import { FaTwitter, FaGithub, FaItchIo } from 'react-icons/fa'
+
+const Footer = () => (
+  <footer className="footer">
+  <div className="content-box footer__container">
+    <Link to="https://www.twitter.com/jayceehex" title="Twitter" target="_blank">
+      <div className="footer__icon">
+        <FaTwitter />
+      </div>
+    </Link>
+    <Link to="https://www.github.com/jayceehex" title="Github" target="_blank">
+      <div className="footer__icon">
+        <FaGithub />
+      </div>
+    </Link>
+    <Link to="https://jayceeha.itch.io" title="Itch.io" target="_blank">
+      <div className="footer__icon">
+        <FaItchIo />
+      </div>
+    </Link>
+  </div>
+</footer>
+)
+
+export default Footer
