0bbd31444b7aaeee91687bce28323d9ab74d84e3
Jen Hoskins <jen.c.hoskins@gmail.com>
2020-10-12 18:07:42 +0100

    Remove SEOTitle component from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import SEO from "../components/seo"
 import '../styles/styles.css'
 import Layout from "../components/layout"
 
@@ -9,7 +8,6 @@ const IndexPage = () => (
     <Helmet>
       <title>jayceehex.github.io</title>
     </Helmet>
-    <SEO title="jayceehex.github.io" />
       <Layout>
         <div className="content-box">
           <p><b>Jen Hoskins</b> is a JavaScript developer from the West Country. They have experience in React and Angular frameworks, and are trying to get better at functional programming. CSS styling is their favourite coding hobby, especially when it comes to accessibility. Outside of code, Jen reads too many books and doesn't get enough vitamin D.</p>
