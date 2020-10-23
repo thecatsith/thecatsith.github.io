e819833dad79e9777b3c4eb07225d03de07e5697
Jen Hoskins <jen.c.hoskins@gmail.com>
2020-10-23 16:19:47 +0100

    Add light/dark mode toggle

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,22 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import "./header.css"
+import { FaLightbulb, FaRegLightbulb } from "react-icons/fa"
 
-const Header = ({ siteTitle }) => (
-  <header className="header content-box">
-    <div>
+const Header = ({ siteTitle, lightMode, onToggleLightMode }) => {
+  const lightModeToggleIcon = () => lightMode ? <FaRegLightbulb /> : <FaLightbulb />
+
+  return (
+    <header className="header content-box">
       <h1 className="header__text">
         <Link to="/">{siteTitle}</Link>
       </h1>
-    </div>
-  </header>
-)
+    <button className="icon-button" onClick={onToggleLightMode} aria-label="Toggle light mode">
+      {lightModeToggleIcon()}
+    </button>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,15 +5,21 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import "./layout.css"
-
 import Header from "./header"
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
+  const [lightMode, setLightMode] = useState(false)
+
+  const toggleLightMode = () => {
+    setLightMode(!lightMode)
+    console.log('lightMode', lightMode)
+  }
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,8 +31,8 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div className="container">
-      <Header siteTitle={data.site.siteMetadata?.title || ``} />
+    <div className={`container${lightMode ? ' light-theme' : ''}`}>
+      <Header siteTitle={data.site.siteMetadata?.title || ``} onToggleLightMode={toggleLightMode} lightMode={lightMode} />
       <main id="main">{children}</main>
       <Footer />
     </div>
