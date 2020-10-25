4010016eb446044da05c68ba4c7db7e0eb735677
Jen Hoskins <jen.c.hoskins@gmail.com>
2020-10-25 18:03:41 +0000

    Format

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,11 @@ import { toggleLightMode } from "../state/app"
 import { connect } from "react-redux"
 
 const NotFoundPage = ({ location, dispatch, isLightMode }) => (
-  <Layout location={location} toggleLightMode={() => dispatch(toggleLightMode(!isLightMode))} lightMode={lightMode}>
+  <Layout
+    location={location}
+    toggleLightMode={() => dispatch(toggleLightMode(!isLightMode))}
+    lightMode={lightMode}
+  >
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
